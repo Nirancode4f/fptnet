ceb8474e680aec6bb5cfc9702a5220b558efcc52
Tran Thanh Nhan <nhan12341184@gmail.com>
2021-12-23 18:40:20 +0700

    Upload and resize image

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const conversation = require("./single_chat/conv/conversation")
 const message = require("./single_chat/mess/message")
 const GroupConversation = require("./group_chat/conv/conversation")
 const GroupMessage = require("./group_chat/mess/message")
-const Post = require("./post/post")
+const Post = require("./post/post");
+const Upload = require("./upload/upload");
 
 
 
@@ -47,6 +48,9 @@ app.use("/api/message", message)
 app.use("/api/group/conversation", GroupConversation)
 app.use("/api/group/message", GroupMessage)
 app.use("/api/post/", Post)
+app.use("/api/upload", Upload);
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`))
