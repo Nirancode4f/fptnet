7ba43c362a8aaff25e4da13ca6f50300b48c7c31
khaangnguyeen <khgammingcraft@gmail.com>
2021-12-24 10:28:12 +0700

    update client login

--- client/src/component/LoginForm.js
+++ client/src/component/LoginForm.js
@@ -1,23 +1,53 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
-export  function LoginForm() {
-    return (
-        <form>
-            <div className='form-inner'>
-                <h2>Login</h2>
-              
-                <div className="form-group">
-                    <label htmlFor='email'>Email:</label>
-                    <input type="email" name='email' id='email' />
-                   
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" name='password' id='password' />
-                </div>
-                
+const LoginForm = (props)=>{
+
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("")
+
+
+
+
+
+    const handleSubmit = () => {
+
+        
+        axios.post('https://fptnetwork.elemarkuspet.repl.co/api/auth/login', {
+            email: email,
+            password: password
+        },{
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+           ) .then(res => {
+                console.log("message: ", res)
+            })
+            .catch(error=>{
+                console.log("error", error)
+            })
+
+
+    }
+
+
+        return (
+            <div>           
+                    <div>
+                        <label >Username</label>
+                        <input id="email" type="text" value={email} onChange={e => setemail(e.target.value)} required />
+                    </div>
+                    <div>
+                        <label >Password</label>
+                        <input id="password" type="password" value={password} onChange={e => setpassword(e.target.value)}  required autoComplete='true'/>
+                    </div>
+                    <div>
+                        <button onClick={handleSubmit}>LOGIN</button>
+                    </div>
             </div>
-            <input type="submit" value="LOGIN" />
-        </form>
-    )
-}
+        )
+    }
+
+export default LoginForm
