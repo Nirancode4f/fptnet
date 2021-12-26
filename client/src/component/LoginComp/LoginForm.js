b968142b8bdce148000ee7db529c0ceffb3f2583
khaangnguyeen <khgammingcraft@gmail.com>
2021-12-26 13:16:52 +0700

    fix done

--- client/src/component/LoginComp/LoginForm.js
+++ client/src/component/LoginComp/LoginForm.js
@@ -11,19 +11,18 @@ const LoginForm = (props) => {
     const [email, setemail] = useState("");
     const [password, setpassword] = useState("")
     const [info, setinfo] = useState("")
-    
 
-    
+
+
 
     const handleSubmit = async (evt) => {
         evt.preventDefault()
         try {
 
-            
+
             const checkpost = (<h1>{info}</h1>)
 
-            
-            
+
             axios.post(`https://fptnetwork.elemarkuspet.repl.co/api/auth/login`, {
                 email: email,
                 password: password
@@ -32,63 +31,69 @@ const LoginForm = (props) => {
                     "Content-Type": "application/json"
                 }
             }
-            
-            ).then((res)=>{
-            
+
+            ).then(async (res) => {
+                if(res.data.message !== "successfully"){
                 setinfo(res.data.message)
-                ReactDOM.render(checkpost,document.getElementById("infor"))
+                ReactDOM.render(checkpost, document.getElementById("infor"))
+                }
                 
+
             })
 
-            .catch(error => {
-                    if(error.request){
+                .catch(error => {
+                    if (error.request) {
                         console.log(error.request)
-                    }if(error.response){
+                    } if (error.response) {
                         console.log(error.response)
                     }
                 })
 
-          
+
         } catch (error) {
             console.log(error)
         }
 
     }
 
-        return (
-            <div className= "login_wrapper">
-                
-                <div className="login_form">
-                    
-                    <h1 className="top_header">Log in</h1>
-                    
-                    <form action="">
-                        <div className="login_type_field">
-                            <input id="email" type="text" value={email} onChange={e => setemail(e.target.value)} required />
-                            <span></span>
-                            <label >Username</label>
-                        </div>
-                        <div className="login_type_field">
-                            <input id="password" type="password" value={password} onChange={e => setpassword(e.target.value)}  required autoComplete='true'/>
-                            <span></span>
-                            <label >Password</label>
-                        </div>
-                        <div>
-                            <div className="login_forgot">Forgot password</div>
-                        </div>
-                        <div>
-                            <button className="login_submit_btn" onClick={handleSubmit}>LOGIN</button>
-                        </div>
-                    </form>
-                    
-                    <div className="login_bottom_suggest">If you not member? <a className="signIn_anchor" href='/register'>Sign in</a></div>
+    return (
+        <div className="login_wrapper">
 
+            <div className="login_form">
+
+                <h1 className="top_header">Log in</h1>
+
+                <form onSubmit={handleSubmit}>
+                    <div className="login_type_field">
+                        <input id="email" type="text" value={email} onChange={e => setemail(e.target.value)} required />
+                        <span></span>
+                        <label >Username</label>
+                    </div>
+                    <div className="login_type_field">
+                        <input id="password" type="password" value={password} onChange={e => setpassword(e.target.value)} required autoComplete='true' />
+                        <span></span>
+                        <label >Password</label>
+                    </div>
+                    <div>
+                        <div id='infor'></div>
+                        <div className="login_forgot">Forgot password</div>
+                    </div>
+                    <div>
+                        <button className="login_submit_btn" >LOGIN</button>
+                    </div>
+                </form>
+
+                <div className="login_bottom_suggest">If you not member?
+                    <Link className="signIn_anchor" to='/register'>Sign in
+                    </Link>
                 </div>
-                
-                <div className="logo_fan"><p>FPT University Academic Network</p></div>
 
             </div>
-        )
-    }
+
+            <div className="logo_fan"><p>FPT University Academic Network</p></div>
+
+        </div>
+    )
+}
 
 export default LoginForm
