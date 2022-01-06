17a2e3063c5ab478c6ebea071c453310e7358d84
khaangnguyeen <khgammingcraft@gmail.com>
2022-01-07 00:55:39 +0700

    mai code tiep

--- client/src/component/Profile/PostPopup/CmtPost/CmtUser.jsx
+++ client/src/component/Profile/PostPopup/CmtPost/CmtUser.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 
-class CmtUser extends React.Component{
+function CmtUser(props) {
 
 
-    render(){
-        return(
+    const { Content,likelist,createAt } = props
 
-            
+    return (
 
-        )
-    }
+        <div className="comment-box">
+            <div className="comment-box-infor-commenter">
+                <div className="comment-box-infor-commenter-avt">
+                    <img src="https://yt3.ggpht.com/ytc/AKedOLR1qNHkmPssXVH0Yr-FeFhNtcvxw9XNYxGOQgWp6g=s48-c-k-c0x00ffffff-no-rj" alt="" />
+                </div>
+                <div className="comment-box-infor-commenter-name">
+                    Tên commenter
+                    <span className="more-info-cmt-btn" > ... </span>
+
+                </div>
+            </div>
+            <div className="comment-box-content">
+                {Content}
+                <div className="reaction-comment">
+                    <span >
+                        Thích
+                    </span>
+                    <span >
+                        Trả lời
+                    </span>
+                </div>
+            </div>
+
+
+        </div>
+
+    )
 }
+
 export default CmtUser
