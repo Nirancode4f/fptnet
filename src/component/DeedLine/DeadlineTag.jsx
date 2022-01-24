3573cc22119fb80363cf1159c8ad57494dd67fa3
khaangnguyeen <khgammingcraft@gmail.com>
2022-01-24 11:24:52 +0700

    update deadline

--- src/component/DeedLine/DeadlineTag.jsx
+++ src/component/DeedLine/DeadlineTag.jsx
@@ -1,33 +1,57 @@
-import { Chip } from '@mui/material';
-import React from 'react';
+import { Chip } from "@mui/material";
+import React from "react";
+import PropTypes from "prop-types";
 
-
-
-function DeadlineTag (props)  {
-
-
-    const {data} = props
-    console.log(data)
-
-    
-
-  return (
-
- <div className='Deadline-Box'>
-
-
-        <div className="row-deadline ">
-          <div className="name-col">       <Chip style={{fontweight:600}} label={data.deadline} color="primary" variant='outlined' /></div>
-          <div className="description-col"><Chip style={{fontweight:600}} label={data.note} color="primary" variant='outlined'/></div>
-          <div className="date-col">       <Chip style={{fontweight:600}} label="20/11/2022" color="success" variant='filled'/> </div>
-      <div className="edit-col"><span className='edit-deadline-btn'> <i className="fas fa-bars"> </i> </span></div>
-      </div>
-
-
-      </div>
-  )
+DeadlineTag.propTypes = {
+  data: PropTypes.object,
+  onHandleClick: PropTypes.func,
 };
 
-export default DeadlineTag
+DeadlineTag.defaultProps = {
+  data: [],
+  onHandleClick: null,
+};
 
+function DeadlineTag(props) {
+  const { data } = props;
+  console.log(data);
 
+  return (
+    <div className="row-deadline ">
+      <div className="name-col">
+        {" "}
+        <Chip
+          style={{ fontweight: 600 }}
+          label={data.deadline}
+          color="primary"
+          variant="outlined"
+        />
+      </div>
+      <div className="description-col">
+        <Chip
+          style={{ fontweight: 600 }}
+          label={data.note}
+          color="primary"
+          variant="outlined"
+        />
+      </div>
+      <div className="date-col">
+        {" "}
+        <Chip
+          style={{ fontweight: 600 }}
+          label="20/11/2022"
+          color="success"
+          variant="filled"
+        />{" "}
+      </div>
+      <div className="edit-col">
+        <span className="edit-deadline-btn">
+          {" "}
+          <i className="fas fa-bars"> </i>{" "}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default DeadlineTag;
