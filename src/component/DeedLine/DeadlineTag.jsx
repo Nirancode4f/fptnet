fefdb5e393e7cd35b3f1777282892cacc79fe7c7
thanhduong1311 <thanhduongjnguyen@gmail.com>
2022-01-24 14:56:43 +0700

    new

--- src/component/DeedLine/DeadlineTag.jsx
+++ src/component/DeedLine/DeadlineTag.jsx
@@ -20,22 +20,12 @@ function DeadlineTag(props) {
     <div className="row-deadline ">
       <div className="name-col">
         {" "}
-        <Chip
-          style={{ fontweight: 600 }}
-          label={data.deadline}
-          color="primary"
-          variant="outlined"
-        />
+          <div> Ch có deadline để test</div>
       </div>
       <div className="description-col">
-        <Chip
-          style={{ fontweight: 600 }}
-          label={data.note}
-          color="primary"
-          variant="outlined"
-        />
+       <div> Ch có deadline để test</div>
       </div>
-      <div className="date-col">
+      <div className="date-col-receive">
         {" "}
         <Chip
           style={{ fontweight: 600 }}
@@ -44,6 +34,17 @@ function DeadlineTag(props) {
           variant="filled"
         />{" "}
       </div>
+
+      <div className="date-col-return">
+        {" "}
+        <Chip
+          style={{ fontweight: 600 }}
+          label="20/11/2022"
+          color="success"
+          variant="filled"
+        />{" "}
+      </div>
+
       <div className="edit-col">
         <span className="edit-deadline-btn">
           {" "}
