5ffcc35a617cf21c676ad3b1cd745469308f35c9
khaangnguyeen <78076796+Nirancode4f@users.noreply.github.com>
2022-01-26 15:26:40 +0700

    new lfs

--- src/component/Layout/NavBar.js
+++ src/component/Layout/NavBar.js
@@ -1,33 +1,55 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
-
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "./assets/logo.jpg";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import SendIcon from "@mui/icons-material/Send";
+import NewspaperOutlinedIcon from "@mui/icons-material/NewspaperOutlined";
+
 function NavBar() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
   return (
     <div className="nav">
       <img src={logo} alt="" className="header__logo" />
-      <div className="nav__add">
-        <Link to="/">
-          <i className="fas fa-plus-circle"></i>
-        </Link>
-      </div>
+
+      {/* connect to ༼ つ ◕_◕ ༽つ https://mui.com/components/stack/ */}
+      <Stack spacing={2}>
+        <Button
+          endIcon={<NewspaperOutlinedIcon style={{ color: "#fff" }} />}
+          fontSize="medium"
+        />
+
+        <Button endIcon={<SendIcon style={{ color: "#f36f21" }} />}></Button>
+
+        <Button endIcon={<SendIcon style={{ color: "#f36f21" }} />}></Button>
+      </Stack>
+
       <div className="nav__news">
         <Link to="/">
           <i className="far fa-newspaper"></i>
         </Link>
       </div>
+
       <div className="nav__chat">
         <Link to="/messenger">
           <i className="far fa-comments"></i>
         </Link>
       </div>
+
       <div className="nav__deadline">
         <Link to="/deadline">
           <i className="fas fa-stopwatch"></i>
         </Link>
       </div>
+
       <div className="nav__profile">
         <Link to="/profile">
           <i className="far fa-user"></i>
