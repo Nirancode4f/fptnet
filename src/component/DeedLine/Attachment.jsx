bb7f40854ea6808fda6826a9fd4662c499b48b20
thanhduong1311 <thanhduongjnguyen@gmail.com>
2022-02-12 21:39:07 +0700

    attachment new

--- src/component/DeedLine/Attachment.jsx
+++ src/component/DeedLine/Attachment.jsx
@@ -13,7 +13,14 @@ const Attachment = (props) => {
     <>
     {data.map((attachment) => (
         <div className="deadline-attachment-row">
-          <Link href={attachment}><GetSeo link={attachment}/></Link>
+          <div className="attachment-background">
+            <img src="https://scontent.fvca1-1.fna.fbcdn.net/v/t1.6435-9/131766102_848176515742014_4155438205270698043_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Qm4aotFwuvwAX_Uns13&_nc_ht=scontent.fvca1-1.fna&oh=00_AT9enx9rMDWxpGrW2KtpA50EB_T8z4wQufkbGyCBROVwVQ&oe=622D7EEE" className='background-attachment-link'/>
+             <div className="attachment-content">Here is your attachment content</div>
+             <div className="attachment-provider">
+               <img src="https://static.xx.fbcdn.net/rsrc.php/yD/r/d4ZIVX-5C-b.ico" className='favicon' />
+               Domain
+               </div>
+          </div>
         </div>
       ))}
 </>
