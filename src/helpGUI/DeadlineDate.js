6c2d8dc79f5581d661563b7b1ac9dbedd29f3a49
khaangnguyeen <khgammingcraft@gmail.com>
2022-03-10 13:37:42 +0700

    date update

--- src/helpGUI/DeadlineDate.js
+++ src/helpGUI/DeadlineDate.js
@@ -3,48 +3,36 @@ import { useEffect, useState } from "react"
 import dateFormat from "dateformat";
 
 
- 
-const DeadlineDate =  {
-  
-    FomatDate : (date) =>{
 
+const DeadlineDate = {
+
+  FomatDate: (date) => {
     const [timeString, settimeString] = useState("")
     const [CmtDate, setCmtDate] = useState(Date())
-
     const d = date
-
-   
     useEffect(() => {
 
-      function TimePost (){
+      function TimePost() {
 
-        let currentyear = dateFormat(timeString,"yyyy")
-        let day = dateFormat(d,"d")
-        let month = dateFormat(d,"m")
-        let year = dateFormat(d, "yyyy")    
-        let hour = dateFormat(d,"h")
-        let minute = dateFormat(d,"MM")
-        let TT = dateFormat(d,"TT")
-        
+        let currentyear = dateFormat(timeString, "yyyy")
+        let day = dateFormat(d, "d")
+        let month = dateFormat(d, "m")
+        let year = dateFormat(d, "yyyy")
+        let hour = dateFormat(d, "h")
+        let minute = dateFormat(d, "MM")
+        let TT = dateFormat(d, "TT")
 
-  
         const fo = `${day}/${month}/${year}`
-        
+
         settimeString(fo)
-      
-    
-
-    }
-
+      }
 
       TimePost()
-    
+
     }, [])
-    
-
-        return {timeString}
-    }
-
- 
+    return { timeString }
   }
+
+
+}
 export default DeadlineDate
